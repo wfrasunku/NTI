c162c45b4c94530542b3e7f0556d2c27d372f4d6
wfrasunku <72517170+wfrasunku@users.noreply.github.com>
2025-09-05 02:06:37 +0200

    Index page

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const layers = [
-  { el: document.getElementById("layer-back"), intensity: 35 },
-  { el: document.getElementById("layer-mid"), intensity: 60 },
-  { el: document.getElementById("layer-front"), intensity: 50 }
+  { el: document.getElementById("layer-back"), intensity: 50 },
+  { el: document.getElementById("layer-front"), intensity: 100 },
+  { el: document.getElementById("layer-objects"), intensity: 100 }
 ];
 
 document.addEventListener("mousemove", (e) => {
@@ -9,8 +9,8 @@ document.addEventListener("mousemove", (e) => {
   const y = e.clientY / window.innerHeight;
 
   layers.forEach(layer => {
-    const moveX = -x * 5 * layer.intensity;
-    const moveY = -y * 2 * layer.intensity;
+    const moveX = -x * 3 * layer.intensity;
+    const moveY = -y * 1.5 * layer.intensity;
     layer.el.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.1)`;
   });
 });
