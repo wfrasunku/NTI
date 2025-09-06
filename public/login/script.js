1b6843952b759134b6baab4677864f6ca3735456
MatMac03 <247727@edu.p.lodz.pl>
2025-09-06 19:04:13 +0200

    poprawiona strona logowania

--- public/login/script.js
+++ public/login/script.js
@@ -5,6 +5,10 @@ async function login() {
     const username = document.getElementById('loginUsername').value;
     const password = document.getElementById('loginPassword').value;
 
+    const message = document.getElementById('message');
+    message.classList.remove('visible');
+    message.innerText = '';
+
     const response = await fetch('http://localhost:3000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -12,19 +16,16 @@ async function login() {
     });
 
     const data = await response.json();
-    const message = document.getElementById('message');
 
     if (response.ok) {
-        // Zapisz u?ytkownika do localStorage
         localStorage.setItem('loggedInUser', username);
-
-        // Przekieruj na stron? g?ówn?
-        window.location.href = '../index.html'; // dostosuj ?cie?k? je?li potrzeba
+        window.location.href = '../index.html';
     } else {
-        message.style.color = 'red';
         message.innerText = data.message;
+        message.classList.add('visible');
     }
 }
+
 
 
 async function register() {
@@ -42,8 +43,26 @@ async function register() {
     const message = document.getElementById('message');
 
     if (response.ok) {
+        const loginForm = document.getElementById('login-form');
+        const registerForm = document.getElementById('register-form');
+
         message.style.color = 'green';
-        message.innerText = data.message;
+        message.innerText = "? Uda?o si? zarejestrowa?! Mo?esz si? zalogowa?.";
+        message.classList.add('visible');
+
+        // Poka? formularz logowania po rejestracji
+        registerForm.style.display = 'none';
+        loginForm.style.display = 'block';
+
+        // Zniknij komunikat po 3 sekundach widoczno?ci + 1 sekunda animacji
+        setTimeout(() => {
+            message.classList.remove('visible'); // animacja opacity 1s
+            setTimeout(() => {
+                message.innerText = ''; // po znikni?ciu czy?cimy tekst
+            }, 1000);
+        }, 3000);
+
+
     } else {
         message.style.color = 'red';
         message.innerText = data.message;
