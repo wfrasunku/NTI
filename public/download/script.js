d156916409a2c2d3ecbef4ee1ec2dc99aed78c4b
MatMac03 <247727@edu.p.lodz.pl>
2025-09-14 18:25:27 +0200

    download cleaned

--- public/download/script.js
+++ public/download/script.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Pobieranie
     const downloadBtn = document.getElementById('download-btn');
     downloadBtn.addEventListener('click', () => {
-        // Mo?esz tu poda? dok?adny link do pliku do pobrania
         window.location.href = '../download/LochMess_1_0_0.zip';
+    });
+
+    // Powi?kszanie obrazków
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    const closeBtn = document.querySelector(".close");
+
+    document.querySelectorAll('.topImages').forEach(img => {
+        img.addEventListener('click', () => {
+            modal.style.display = "block";
+            modalImg.src = img.src;
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = "none";
+    });
+
+    // Zamknij po klikni?ciu w t?o
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
     });
 });
